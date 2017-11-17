d432cd20eda9bd46f65c7ce56fac211431b91960
J. Jonatan Rojas R. <jjrrdecali90@gmail.com>
2017-11-16 23:07:20 -0500

    esta es mi segunda prueba

--- src/safcon/SAFCON.java
+++ src/safcon/SAFCON.java
@@ -6,10 +6,10 @@ public class SAFCON {
 
    
     public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Aqui falto el hpta Negro(Cristian)");
         JOptionPane.showMessageDialog(null, "Esta es mi parte(Sotelo)");
         JOptionPane.showMessageDialog(null, "Hola a todos (SeRoGra)");
         JOptionPane.showMessageDialog(null, "Esta es mi parte(Jonatan)");
 
-    }
-    
+    }       
 }
