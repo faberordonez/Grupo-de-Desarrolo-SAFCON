09a9b37b424b4cd2461b26e66340bcb10a177c3e
J. Jonatan Rojas R. <jjrrdecali90@gmail.com>
2017-11-18 17:18:08 -0500

    elimina main

--- src/safcon/SAFCON.java
+++ src/safcon/SAFCON.java
@@ -5,11 +5,14 @@ import javax.swing.JOptionPane;
 public class SAFCON {
 
    
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Aqui falto el hpta Negro(Cristian)");
-        JOptionPane.showMessageDialog(null, "Esta es mi parte(Sotelo)");
-        JOptionPane.showMessageDialog(null, "Hola a todos (SeRoGra)");
-        JOptionPane.showMessageDialog(null, "Esta es mi parte(Jonatan)");
+//    public static void main(String[] args) {
+//        GestionDeNotasXActividadesYXLogro gs=new GestionDeNotasXActividadesYXLogro();
+//        gs.setVisible(true);
+//        
+//        JOptionPane.showMessageDialog(null, "Aqui falto el hpta Negro(Cristian)");
+//        JOptionPane.showMessageDialog(null, "Esta es mi parte(Sotelo)");
+//        JOptionPane.showMessageDialog(null, "Hola a todos (SeRoGra)");
+//        JOptionPane.showMessageDialog(null, "Esta es mi parte(Jonatan)");
 
-    }       
+//    }       
 }
